8fbbadbc61e7e4766886fb7f9936eb093fb83786
Asass <asassinev@yandex.ru>
2018-09-20 22:09:21 +0300

    interface API

--- app/src/main/java/com/example/asass/firstcs/ServerApi.java
+++ app/src/main/java/com/example/asass/firstcs/ServerApi.java
@@ -2,12 +2,14 @@ package com.example.asass.firstcs;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface ServerApi {
 
-    @GET("/get_user_details")
-    Call<User> getUserDetails(@Query("login") String UserName, @Query("password") String Password);
+    @GET("/getUser")
+    Call<User> getUser(@Query("login") String UserName, @Query("password") String Password);
+
+    @GET("/createUser")
+    Call<User> createUser(@Query("login") String UserName, @Query("password") String Password);
 
 }
