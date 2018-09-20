9b9ff0365932e90769832850fb90f3a27dbb5bf7
Asass <asassinev@yandex.ru>
2018-09-20 22:09:47 +0300

    add registration (server-db)

--- app/src/main/java/com/example/asass/firstcs/Registration.java
+++ app/src/main/java/com/example/asass/firstcs/Registration.java
@@ -16,26 +16,13 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Registration extends Activity {
-
-    private static final String URL = "https://localhost";
-    private Gson gson = new GsonBuilder().create();
-
-    private Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-    private ServerApi intf = retrofit.create(ServerApi.class);
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
@@ -46,26 +33,69 @@ public class Registration extends Activity {
 
     private ProgressDialog pDialog;
 
-    Button regbut;
-    EditText login, password;
+    Button Regbut;
+    EditText Login, Password;
 
 
-    private static String url_create_user = "http://localhost/create_user.php";
-
-    //private static final String TAG_SUCCESS = "success";
+    private static String BASE_URL = "http://79.139.231.194:10202";
+    private String login,password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
 
-        regbut = (Button) findViewById(R.id.regbut);
-        login = (EditText) findViewById(R.id.login);
-        password = (EditText) findViewById(R.id.password);
+        Regbut = (Button) findViewById(R.id.regbut);
+        Login = (EditText) findViewById(R.id.login);
+        Password = (EditText) findViewById(R.id.password);
 
-        regbut.setOnClickListener(new View.OnClickListener() {
+        Regbut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (isNetworkAvailable()) {
+                    login = Login.getText().toString();
+                    password = Password.getText().toString();
+                    if (login.equals("") || password.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Введите логин и пароль.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Gson gson = new GsonBuilder()
+                                .setLenient()
+                                .create();
 
+                        Retrofit retrofit = new Retrofit.Builder()
+                                .baseUrl(BASE_URL)
+                                .addConverterFactory(GsonConverterFactory.create(gson))
+                                .build();
+
+                        ServerApi reg = retrofit.create(ServerApi.class);
+                        reg.createUser(login, password).enqueue(new Callback<User>() {
+                            @Override
+                            public void onResponse(Call<User> call, Response<User> response) {
+                                if (response.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Добро пожаловать!", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Registration.this, Profile.class);
+                                    startActivity(intent);
+                                } else {
+                                    switch(response.code()) {
+                                        case 400:
+                                            Toast.makeText(getApplicationContext(), "Логин занят!", Toast.LENGTH_LONG).show();
+                                            break;
+                                        case 500:
+                                            // ошибка на сервере. можно использовать ResponseBody, см. ниже
+                                            break;
+                                    }
+                                    System.out.println(response.errorBody());
+                                }
+                            }
+                            @Override
+                            public void onFailure(Call<User> call, Throwable t) {
+                                Toast.makeText(Registration.this, "An error occurred during networking", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } else
+                {
+                    Toast.makeText(getApplicationContext(), "Подключите интернет!", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
