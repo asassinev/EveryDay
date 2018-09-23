d8d2e75a9cfbec1f3f56cffc80db3535c21c3452
Asass <asassinev@yandex.ru>
2018-09-23 16:00:37 +0300

    add func createText, getText

--- app/src/main/java/com/example/asass/firstcs/ServerApi.java
+++ app/src/main/java/com/example/asass/firstcs/ServerApi.java
@@ -1,7 +1,11 @@
 package com.example.asass.firstcs;
 
+import java.util.Map;
+
 import retrofit2.Call;
+import retrofit2.http.FieldMap;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ServerApi {
@@ -12,4 +16,9 @@ public interface ServerApi {
     @GET("/createUser")
     Call<User> createUser(@Query("login") String UserName, @Query("password") String Password);
 
+    @POST("/createText")
+    Call<message> createText(@FieldMap Map<String, String> text);
+
+    @GET("/getText")
+    Call<message> getText(@Query("Author") String Author);
 }
