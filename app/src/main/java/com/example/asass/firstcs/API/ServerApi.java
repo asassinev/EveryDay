11b16fc73999e50bf480d5b652421dd5733bb5e7
asassinev <34071242+asassinev@users.noreply.github.com>
2018-10-03 20:35:13 +0300

    Update Interface

--- app/src/main/java/com/example/asass/firstcs/API/ServerApi.java
+++ app/src/main/java/com/example/asass/firstcs/API/ServerApi.java
@@ -1,13 +1,17 @@
 package com.example.asass.firstcs.API;
 
+import com.example.asass.firstcs.model.Token;
 import com.example.asass.firstcs.model.Tweet;
 import com.example.asass.firstcs.model.User;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
@@ -16,14 +20,15 @@ import retrofit2.http.Query;
 public interface ServerApi {
 
     @GET("/user/get")
-    Call<User> getUser(@Query("login") String UserName, @Query("password") String Password);
+    Call<Token> getUser(@Query("login") String UserName, @Query("password") String Password);
 
     @GET("/user/create")
     Call<User> createUser(@Query("login") String UserName, @Query("password") String Password, @Query("email") String email);
 
+    @FormUrlEncoded
     @POST("/text/create")
-    Call<Tweet> createText(@FieldMap Map<String, String> text,  @Header("access_token") String access_token);
+    Call<Tweet> createText(@FieldMap Map<String, String> map, @Header("Authorization") String accessToken);
 
     @GET("/text/get")
-    Call<List<Tweet>> getTweets(@Query("login") String Login);//,  @Header("access_token") String access_token);
+    Call<List<Tweet>> getTweets(@Query("login") String Login,  @Header("Authorization") String accessToken);
 }
