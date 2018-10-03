42c78a586221af4e3fe37c6b0805e4d8d377e206
asassinev <34071242+asassinev@users.noreply.github.com>
2018-10-03 20:33:52 +0300

    Update

--- app/src/main/java/com/example/asass/firstcs/model/User.java
+++ app/src/main/java/com/example/asass/firstcs/model/User.java
@@ -3,6 +3,9 @@ package com.example.asass.firstcs.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class User {
 
     @SerializedName("email")
@@ -14,28 +17,6 @@ public class User {
     @SerializedName("password")
     @Expose
     private String password;
-    @SerializedName("access_token")
-    @Expose
-    private static String access_token = "access_token";
-    @SerializedName("refresh_token")
-    @Expose
-    private static String refresh_token = "refresh_token";
-
-    public String getAccessToken() {
-        return access_token;
-    }
-
-    public void setAccessToken(String access_token) {
-        this.access_token = access_token;
-    }
-
-    public String getRefreshToken() {
-        return refresh_token;
-    }
-
-    public void setRefreshToken(String refresh_token) {
-        this.refresh_token = refresh_token;
-    }
 
     public String getEmail() {
         return email;
